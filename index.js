9d89ae270ab49353a729894a0857be313fdeb235
Verlic <hernan.meydac@auth0.com>
2019-11-22 20:59:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const routines = require("./routines.json")
+
+app.get('/routines/:id', function(req, res){
+const routine = routines.find(routine => routine.id == req.params.id)
+if(routine){
+    res.json(routine)
+}
+else{
+    res.status(404).end()
+}
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
